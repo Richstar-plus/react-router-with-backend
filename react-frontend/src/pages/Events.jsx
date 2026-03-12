d35473ab27ef245af02122b27265fdb0a5a44799
richard <richardsunday0812@gmail.com>
2026-03-12 16:40:21 +0100

    Implement event listing in EventsPage with dummy data

--- react-frontend/src/pages/Events.jsx
+++ react-frontend/src/pages/Events.jsx
@@ -1,3 +1,29 @@
+import { Link } from "react-router-dom";
+
+const DUMMY_EVENTS = [
+  {
+    id: "e1",
+    title: "Event 1",
+    description: "Description for Event 1",
+  },
+  {
+    id: "e2",
+    title: "Event 2",
+    description: "Description for Event 2",
+  },
+];
+
 export function EventsPage() {
-  return <h1>Events Page</h1>;
+  return (
+    <>
+      <h1>Events Page</h1>
+      <ul>
+        {DUMMY_EVENTS.map((event) => (
+          <li key={event.id}>
+            <Link to={event.id}>{event.title}</Link>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 }
