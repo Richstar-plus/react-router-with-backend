b865a579482363185b8d3e3bdfa5040d7f21561c
richard <richardsunday0812@gmail.com>
2026-03-13 11:33:02 +0100

    Refactor event routes for consistent formatting and remove unnecessary delay in GET /events

--- react-frontend/src/pages/Root.jsx
+++ react-frontend/src/pages/Root.jsx
@@ -2,10 +2,12 @@ import { Outlet } from "react-router-dom";
 import MainNavigation from "../components/MainNavigation";
 
 export function RootLayout() {
+  // const navigation = useNavigation();
   return (
     <div>
       <MainNavigation />
       <main>
+        {/* {navigation.state === "loading" && <p>Loading...</p>} */}
         <Outlet />
       </main>
     </div>
