ebd1576f4c8cfeb463ccb67baf1657224f155047
richard <richardsunday0812@gmail.com>
2026-03-14 11:53:34 +0100

    Add ErrorPage component and integrate error handling in routing

---
+++ react-frontend/src/pages/Error.jsx
@@ -0,0 +1,24 @@
+import  {useRouteError}  from "react-router-dom";
+
+import  PageContent  from "../components/PageContent";
+
+export function ErrorPage() {
+  const error = useRouteError();
+  let title = "An error occurred!";
+  let message = "Sorry, an unexpected error has occurred.";
+
+  if(error.status === 500) {
+    message = JSON.parse(error.data).message;
+  }
+
+  if(error.status === 404) {
+    title = "Not found!";
+    message = "Could not find resource or page.";
+  }
+
+  return (
+    <PageContent title={title}>
+      <p>{message}</p>
+    </PageContent>
+  );
+}
--- react-frontend/src/pages/Events.jsx
+++ react-frontend/src/pages/Events.jsx
@@ -4,15 +4,21 @@ import EventsList from "../components/EventsList";
 
 export function EventsPage() {
   const fetchedEvents = useLoaderData();
+  // if(fetchedEvents.isError == true) {
+  //   return <p>{fetchedEvents.message}</p>;
+  // }
 
   return <>{<EventsList events={fetchedEvents} />}</>;
 }
 
 export async function Loader() {
-  const response = await fetch("http://localhost:8080/events");
+  const response = await fetch("http://localhost:8080/event");
 
   if (!response.ok) {
-    // Handle error
+    // return {isError: true, message: "Could not fetch events."};
+    throw new Response(JSON.stringify({ message: "Could not fetch events." }), {
+      status: 500,
+    });
   } else {
     const resData = await response.json();
     return resData.events;
