4300d635317a685e3b1c7ce1dc9c4398dea0b856
richard <richardsunday0812@gmail.com>
2026-04-01 17:05:38 +0100

    Enhance EventForm to pre-fill fields with existing event data in EditEventPage

--- react-frontend/src/pages/EditEvent.jsx
+++ react-frontend/src/pages/EditEvent.jsx
@@ -1,7 +1,11 @@
 import  EventForm  from "../components/EventForm";
+import { useRouteLoaderData } from "react-router-dom";
 
 export function EditEventPage() {
+  const data = useRouteLoaderData('event-details');
+  const fetchedEvent = data.event;
+
   return (
-    <EventForm />
+    <EventForm event={fetchedEvent} />
   );
 }
